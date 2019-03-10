54273e020a12d52ab021bffd551d49bd86f5666b
Denser <denser.ru@gmail.com>
2019-03-10 12:40:17 +0700

    русский язык для экрана
    Найдено на https://habr.com/ru/post/408197/
    Использовать: outStr вместо print

--- libraries/LiquidCrystal_I2C/LiquidCrystal_I2C.h
+++ libraries/LiquidCrystal_I2C/LiquidCrystal_I2C.h
@@ -79,7 +79,7 @@ public:
   void createChar(uint8_t, uint8_t[]);
   void createChar(uint8_t location, const char *charmap);
   // Example: 	const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
-  
+  void outStr(char str[]);  
   void setCursor(uint8_t, uint8_t); 
 #if defined(ARDUINO) && ARDUINO >= 100
   virtual size_t write(uint8_t);
